2ec9b28c00e343e27f6ece8270c5b12a5e7132e6
Jiangyuan Li <jerry@JiangyuandeMacBook-Air.local>
2025-03-22 00:52:22 -0400

    Updated Mar 21 2025

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """
-    Author: Jerry Li, Generative AI
-    Date: 2025-01-25
+    Author: Jerry Li
+    Date: 2025-03-22
     Version: 0.1
 """
 
@@ -27,22 +27,9 @@ def serve_page(page_name):
         with open(page_path, "r") as file:
             file = open(page_path, encoding="utf8")
             content = file.read()
-        return render_template("layout.html", content=content)
+        return render_template("page.html", content=content)
     else:
         abort(404)  # Return a 404 error if the page doesn't exist
-
-
-# @app.route("/pages/subpages/<subpage_name>.html")
-# def serve_subpage(subpage_name):
-#     subpage_path = os.path.join("pages", "subpages", f"{subpage_name}.html")
-#     if os.path.exists(subpage_path):
-#         # Read the page content here and pass it to the template
-#         with open(subpage_path, "r") as file:
-#             file = open(subpage_path, encoding="utf8")
-#             content = file.read()
-#         return render_template("dynamic_subpage.html", content=content)
-#     else:
-#         abort(404)  # Return a 404 error if the subpage doesn't exist
 
 
 freezer = Freezer(app)
@@ -58,16 +45,6 @@ def serve_page():
         if file_name.endswith(".html"):
             # Pass the page name without '.html'
             yield {"page_name": file_name[:-5]}
-
-
-# @freezer.register_generator
-# def serve_subpage():
-#     # List all files in the 'pages/subpages' directory
-#     subpages_dir = os.path.join("pages", "subpages")
-#     for file_name in os.listdir(subpages_dir):
-#         if file_name.endswith(".html"):
-#             # Pass the subpage name without '.html'
-#             yield {"subpage_name": file_name[:-5]}
 
 
 if __name__ == "__main__":
